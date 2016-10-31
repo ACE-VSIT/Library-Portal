051037e30da6fa522fb7c264c17649adaf70944c
Ashish <ashishpahwa7@gmail.com>
2016-10-31 14:15:02 +0530

    added authentication method

--- acelibraryapp/views.py
+++ acelibraryapp/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 
 def home_page(request):
 	return render(request, 'acelibraryapp/home_page.html', {})
+
+
+@login_required
+def Tasks(request):
+	return render(request,'acelibraryapp/tasks.html',{})
+
+
+def member_login(request):
+
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return redirect('acelibraryapp.views.Tasks')
+			else:
+				print("Disabled account")
+		else:
+			print("Invalid login")
+
+	return render(request, 'acelibraryapp/login.html', {})
