841f611da85f6f4e60b8b3bd2627fb26ce204c26
DesmondANIMUS <desmondmiles36@gmail.com>
2016-11-02 19:55:52 +0530

    Attendance model fix and resource model addition

--- acelibraryapp/models.py
+++ acelibraryapp/models.py
@@ -24,16 +24,31 @@ class Problem(models.Model):
         return self.title
 
 class Attendance(models.Model):
-    person_name = models.CharField(max_length=200)        
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    agenda = models.CharField(max_length=200)
-    event_name = models.CharField(max_length=200)
+    person_name = models.CharField(max_length=200)                
     attendance_status =   models.BooleanField(default=False)                                   
+
+    def _str_(self):
+        return self.attendance_status        
+
+
+class EventNameDate(models.Model):            
+    event_name = models.CharField(max_length=200)
+				date_id	=	models.integerfield(default=0)
+				created_date = models.DateTimeField(
+            default=timezone.now)
 
     def record(self):
         self.created_date = timezone.now()
         self.save()
 
+class Resource(models.Model):
+    author = models.ForeignKey('auth.User')                
+    resource_category = models.CharField(max_length=200)
+    resource_title = models.CharField(max_length=200)
+				resource_desc = models.CharField(max_length=20000)    
+    resource_approval =   models.BooleanField(default=False)
+				resource_id	=	models.integerfield(default=0)						                                   
+
     def _str_(self):
-        return self.attendance_status        
+        return self.resource_approval        
+        
