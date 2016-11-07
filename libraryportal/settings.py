dae7213be84d8cfea28c90c0800125e67d96a677
Ashish <ashishpahwa7@gmail.com>
2016-11-07 20:20:53 +0530

    support for social auth

--- libraryportal/settings.py
+++ libraryportal/settings.py
@@ -34,10 +34,13 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'social.apps.django_app.default',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'acelibraryapp',
+
+  
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -121,3 +124,23 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'social.apps.django_app.context_processors.backends',
+    'social.apps.django_app.context_processors.login_redirect',
+)
+
+AUTHENTICATION_BACKENDS = (
+    'social.backends.facebook.FacebookOAuth2',
+    'social.backends.google.GoogleOAuth2',
+    'social.backends.twitter.TwitterOAuth',
+    'django.contrib.auth.backends.ModelBackend',
+)
