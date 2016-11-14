27e1b641aec7a1ccf53e3dedf7f1e5dc9865f7c6
Ashish Pahwa <ashishpahwa7@gmail.com>
2016-11-14 20:29:32 +0530

    update

--- acelibraryapp/admin.py
+++ acelibraryapp/admin.py
@@ -3,5 +3,25 @@ from django.contrib import admin
 admin.site.site_title = 'ACE VIRTUAL LIBRARY'
 admin.site.site_header = 'ACE ADMIN PANEL'
 
+<<<<<<< HEAD
+from .models import Tasks,Resources, Categories
 
 # Register your models here.
+admin.site.register(Tasks)
+#admin.site.register(Resources)
+admin.site.register(Categories)
+
+class SampleAdmin(admin.ModelAdmin):
+	pass
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('Category','Course','Author','Difficulty','URL', 'approval_status')
+
+admin.site.register(Resources, BookAdmin)
+=======
+from .models import Tasks
+
+# Register your models here.
+admin.site.register(Tasks)
+>>>>>>> 1eaccff4ddf20d6f6006902dcb5ebd5ec147acec
--- acelibraryapp/views.py
+++ acelibraryapp/views.py
@@ -8,114 +8,150 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.views import View
 import json
+<<<<<<< HEAD
+from .models import Tasks, Resources, Categories
+=======
+from .models import Tasks
 
+>>>>>>> 1eaccff4ddf20d6f6006902dcb5ebd5ec147acec
 # Create your views here.
 
 class Authentication(View):
-
-	def __init__(self):
-		
-		self.name = ''
-		self.email = ''
-		self.uid = ''
-		self.valid = 4		
-
+	
+	data = {}
+	
 	''' Test if user is authorized to access library resources '''
 	def isMember(self,name):	
 		
 		with open('acelibraryapp/ace_membors.json') as json_data:
 			data = json.load(json_data)
-
 		data = (item for item in data if item["name"] == name).next()
 		if (data['valid'] ==3 or data['valid']==4) and name == data['name']:
 			return True
-		
 		return False
 
 	def fetchDetails(self,name):
 
 		with open('acelibraryapp/ace_membors.json') as json_data:
 			data = json.load(json_data)
-
 		data = (item for item in data if item["name"] == name).next()
 		if data['valid']==3: data['valid']='Core Member'
 		if data['valid']==4: data['valid']='Member'
 		return data
 
 	def get(self, request):
-
 		#name = str(request.user.first_name) + " " + str(request.user.last_name) #throws exception 
 		if request.user.is_active and self.isMember(str(request.user.first_name) + " " + str(request.user.last_name)):
+			Authentication.data = self.fetchDetails(str(request.user.first_name) + " " + str(request.user.last_name)) 			
 			return redirect('/home')
 		else :
 			auth_logout(request)
+<<<<<<< HEAD
+		return render(request,'acelibraryapp/index.html',{'user': request.user})
+
+
+class HomePage(Authentication):
+	
+	
+
+=======
 		return render(request,'acelibraryapp/index.html',{'user': request.user,'user': request.user})
 
 
 class HomePage(Authentication):
 
-	def __init__(self):
-		self.name=''
 	
+>>>>>>> 1eaccff4ddf20d6f6006902dcb5ebd5ec147acec
 	def get(self, request):
 
 		name = str(request.user.first_name) + " " + str(request.user.last_name)
-
+		#print Authentication.data
 		if not self.isMember(name):
 			auth_logout(request)
 			return redirect('/')
-			
-		return render(request, 'acelibraryapp/home.html', self.fetchDetails(name))
+<<<<<<< HEAD
+		
+		name = str(request.user.first_name) + " " + str(request.user.last_name)
+		auth = Authentication()
+		self.valid = Authentication.fetchDetails(auth,name)['valid']
 
 
+		return render(request, 'acelibraryapp/home.html', {'valid':self.valid})
 
-class Resources(HomePage):
+
+class ResourceView(Authentication):
+
+
+	def fetch_categories(self):
+		categories = Categories.objects.all()
+		return categories
+
+	def get(self, request):
+
+		categories = self.fetch_categories()
+		name = str(request.user.first_name) + " " + str(request.user.last_name)
+		auth = Authentication()
+		self.valid = Authentication.fetchDetails(auth,name)['valid']
+
+		return render(request,'acelibraryapp/resource.html',{'valid':self.valid, 'categories' : categories})
+=======
+
+		return render(request, 'acelibraryapp/home.html', Authentication.data)
+
+
+class Resources(Authentication):
 
 	def fetch_resouces():
 		pass
 
-
 	def get(self, request):
-
-		name = str(request.user.first_name) + " " + str(request.user.last_name)
-
-		return render(request,'acelibraryapp/resource.html',self.fetchDetails(name))
+		return render(request,'acelibraryapp/resource.html',Authentication.data)
+>>>>>>> 1eaccff4ddf20d6f6006902dcb5ebd5ec147acec
 
 
+class TaskView(Authentication):
 
+<<<<<<< HEAD
 
-'''
-@login_required(login_url='/')
-def resource(request):
-
-	# Insert query to get approved resource
-	name = str(request.user.first_name) + " " + str(request.user.last_name)
-	auth = Authentication()
-
-	return render(request,'acelibraryapp/resource.html',Authentication.fetchDetails(auth,name))
-
-'''
-
-
-@login_required(login_url='/')
-def python(request):
-
-	name = str(request.user.first_name) + " " + str(request.user.last_name)
-	auth = Authentication()
-
-	return render(request,'acelibraryapp/python.html',Authentication.fetchDetails(auth,name))
-
-
-class TaskView(HomePage):
-
-	def get_tasks():  
-		pass
+=======
+>>>>>>> 1eaccff4ddf20d6f6006902dcb5ebd5ec147acec
+	def fetch_tasks(self):  
+		tasks = Tasks.objects.filter(approval_status=True)
+		return tasks
 
 	def get(self, request):
 		
+<<<<<<< HEAD
+		tasks = self.fetch_tasks()
 		name = str(request.user.first_name) + " " + str(request.user.last_name)
+		
+		auth = Authentication()
+		self.valid = Authentication.fetchDetails(auth,name)['valid']
+		
+		return render(request,'acelibraryapp/tasks.html',{'valid':self.valid, 'tasks':tasks})
 
-		return render(request,'acelibraryapp/tasks.html',self.fetchDetails(name))
+
+
+class ResourceDetails(ResourceView):
+
+
+	def fetch_resource(self,string):
+		resource = get_object_or_404(Resources, Difficulty=string)
+		return resource
+
+	def get(self, request):
+
+		name = str(request.user.first_name) + " " + str(request.user.last_name)
+		auth = Authentication()
+		self.valid = Authentication.fetchDetails(auth,name)['valid']
+
+		return render(request, 'acelibraryapp/resource_detail.html',{'valid':self.valid})
+
+=======
+		Authentication.data['tasks']= self.fetch_tasks()
+		
+		return render(request,'acelibraryapp/tasks.html',Authentication.data)
+>>>>>>> 1eaccff4ddf20d6f6006902dcb5ebd5ec147acec
 
 '''
 @login_required(login_url='/')
@@ -146,3 +182,27 @@ def tasks(request):
 	return render(request,'acelibraryapp/tasks.html',{})
 
 '''
+<<<<<<< HEAD
+'''
+=======
+
+>>>>>>> 1eaccff4ddf20d6f6006902dcb5ebd5ec147acec
+@login_required(login_url='/')
+def python(request):
+
+	name = str(request.user.first_name) + " " + str(request.user.last_name)
+	auth = Authentication()
+
+	return render(request,'acelibraryapp/python.html',Authentication.fetchDetails(auth,name))
+<<<<<<< HEAD
+'''
+
+
+def resource_details(request, pk):
+	
+	category = get_object_or_404(Categories, pk=pk)
+	resources = Resources.objects.filter(Category=category)	
+	return render(request, 'acelibraryapp/resource_detail.html', {'resources': resources, 'category':category})
+
+=======
+>>>>>>> 1eaccff4ddf20d6f6006902dcb5ebd5ec147acec
